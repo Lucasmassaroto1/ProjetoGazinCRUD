50687563ac27cd2d5da23d19670848036b61143f
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-07-15 08:21:33 -0300

    Tirado card

--- includes/controllers/Controller.configuracoes.php
+++ includes/controllers/Controller.configuracoes.php
@@ -23,25 +23,4 @@
     $cargo_auto = $_SESSION['cargo_auto'] ?? '@Membro'; // padrão
     $mensagemComCargo = str_replace('{user.mention}', '<span class="cargo">' . htmlspecialchars($cargo_auto) . '</span>', $mensagemOriginal);
 
-    /* // ================ MUSICA E VOLUME ================
-    $stmtmusic = $conexao->prepare("SELECT m.*, s.nome AS nome_status FROM musica m JOIN status s ON m.id_status = s.id WHERE usuario_id = ? AND m.id_status IN (1, 2) ORDER BY m.id_status ASC, m.id ASC");
-    $stmtmusic->execute([$usuario_id]);
-    $musica = $stmtmusic->fetchAll(PDO::FETCH_ASSOC);
-
-
-    $usuario_id = $_SESSION['usuario_id'];
-
-    $stmtVolume = $conexao->prepare("SELECT volume FROM configuracoes WHERE usuario_id = :usuario_id");
-    $stmtVolume->execute([':usuario_id' => $usuario_id]);
-
-    $volume = $stmtVolume->fetchColumn();
-    if($volume === false){
-        $volume = 50;
-
-        $stmtInserir = $conexao->prepare("INSERT INTO configuracoes (usuario_id, volume) VALUES (:usuario_id, :volume)");
-        $stmtInserir->execute([
-            ':usuario_id' => $usuario_id,
-            ':volume' => $volume
-        ]);
-    } */
 ?>
